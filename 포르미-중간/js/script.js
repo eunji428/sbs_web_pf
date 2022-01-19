ac41107d733b0616b1a5688d5251b36cdca9aedb
[eunji428] <[eunji428@nate.com]>
2022-01-19 17:51:14 +0900

    포르미-3일차

--- 포르미-중간/js/script.js
+++ 포르미-중간/js/script.js
@@ -1,7 +1,30 @@
 $(function() {
 
+    // 메인 슬라이드
     $('.slide-container').slick({
         dots: true,
+        arrows: false,
+
+        autoplay: true,                 // 자동재생 여부
+        autoplaySpeed: 3000,            // 자동재생 슬라이드 시간
+        infinite: true,                 // 무한 반복 
+        speed:800,                      // 슬라이드가 전환되는 시간
+
+
+        // centerMode: true,            // 센터 모드
+        // centerPadding: '60px',       // 센터 모드 시, 내부여백
+        slidesToShow: 1,                // 보여질 슬라이드 개수
+        slidesToScroll: 1,              // 스크롤될 슬라이드 개수
+
+
+        pauseOnDotsHover: true,         // 페이지네이션 호버 시 자동재생 멈춤
+        pauseOnFocus: true,             // 포커스 시, 자동재생 멈춤
+        pauseOnHover: true,             // 호버 시, 자동재생 멈춤
+    })
+
+    // 인기 상품 슬라이드
+    $('.product-slide').slick({
+        dots: false,
         arrows: true,
 
         autoplay: true,                 // 자동재생 여부
@@ -12,8 +35,8 @@ $(function() {
 
         // centerMode: true,            // 센터 모드
         // centerPadding: '60px',       // 센터 모드 시, 내부여백
-        slidesToShow: 1,                // 보여질 슬라이드 개수
-        slidesToScroll: 1,              // 스크롤될 슬라이드 개수
+        slidesToShow: 4,                // 보여질 슬라이드 개수
+        slidesToScroll: 4,              // 스크롤될 슬라이드 개수
 
 
         pauseOnDotsHover: true,         // 페이지네이션 호버 시 자동재생 멈춤
